d14aec13511dfbe5270a917788e6ba98883f2471
vineet-cyberkan <vineet1.st@gmail.com>
2019-05-24 16:49:29 +0530

    bootstrap added and called into app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+  '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+  './app.component.scss'
+  ]
 })
 export class AppComponent {
-  title = 'ngApp6';
+  title = 'Angular 7 test app';
 }
